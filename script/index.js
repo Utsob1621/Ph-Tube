4f1ca6585ce525af968176162987d414fdc4eeda
Utsob1621 <utsobsaha9072@gmail.com>
2026-03-19 23:29:48 +0600

    implement search input box or fetching and rendering from API

--- script/index.js
+++ script/index.js
@@ -11,16 +11,15 @@ function loadCategories (){
   .then(data => displayCategories(data.categories))
 }
 
-const loadVideos = () => {
-  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+const loadVideos = (searchText = "") => {
+  fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`)
   .then(Response => Response.json())
   .then(data => {
     removeActiveClass();
-    const allBtn = document.getElementById("btn-all");
-      allBtn.classList.add("active");
+
+      document.getElementById("btn-all").classList.add("active");
       displayVideos(data.videos);
 
-  
   })
   removeActiveClass();
 }
@@ -40,6 +39,37 @@ const loadCategoryVideos = (id) => {
       displayVideos(data.category)
       
     })
+}
+
+const loadVideoDetails = (videoId) => {
+  
+
+  const url = `
+  https://openapi.programming-hero.com/api/phero-tube/video/${videoId}
+  `;
+  fetch(url)
+  .then(res => res.json())
+  .then(data => displayVideoDetails(data.video));
+}
+
+const displayVideoDetails = (video) => {
+  console.log(video)
+  document.getElementById("video_details").showModal()
+  const detailsContainer = document.getElementById("details-container")
+   detailsContainer.innerHTML =`
+   <div class="card bg-base-100 image-full shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+  </div>
+</div>
+   `
+
 }
 
 
@@ -95,16 +125,21 @@ const displayVideos = (videos) => {
       <div class="intro">
       <h2 class="text-sm font-semibold">${video.title}</h2>
       <p class="flex gap-2 text-gray-400">${video.authors[0].profile_name} 
-        <img class="w-5 h-5" src="./assets/icons8-verified-48.png" alt=""></p>
+        ${video.authors[0].verified == true ? `<img class="w-5 h-5" src="./assets/icons8-verified-48.png" alt=""></p>` : ``}
       <p class="text-gray-400">${video.others.views} views</p>
       </div>
     </div>
+    <button onClick = loadVideoDetails("${video.video_id}") class="btn btn-block">Show details</button>
   </div>
     `
     videoContainer.appendChild(videoCard);
   });
-
 } 
+
+document.getElementById("search-input").addEventListener("keyup", (e) => {
+  const input = e .target.value;
+  loadVideos(input);
+})
 
 
 loadCategories();
